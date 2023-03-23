8922caeb6f1e2ee6680e331ae97d45875f8fd019
Professor Lozano <prof-luiz@outlook.com>
2023-03-23 09:33:12 -0300

    criação do exemplo 18

---
+++ Aula06 - 23.03/exemplo18.js
@@ -0,0 +1,22 @@
+class Livro{
+    constructor(pNome, pPreco){
+        this.nome = pNome
+        this.preco = pPreco
+    }
+
+    get Nome(){return this.nome}
+    set Nome(pNome){this.nome = pNome}
+
+    get Preco(){return this.preco}
+    set Preco(pPreco){this.preco = pPreco}
+   
+   calcularDesconto(pDesconto){
+    this.preco = this.preco - ((this.preco * pDesconto) / 100)
+   }
+}
+
+var obj_Livro1 = new Livro("Use a cabeça Java", 200)
+obj_Livro1.calcularDesconto(30)
+console.log("Nome do Livro: " + obj_Livro1.nome + " custa " + obj_Livro1.preco + " reais")
+
+
