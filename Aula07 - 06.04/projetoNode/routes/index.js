699c77853aac375c19197ae86b306de5f2e4252b
Professor Lozano <prof-luiz@outlook.com>
2023-04-13 10:23:02 -0300

    Conteudo da aula09

--- Aula07 - 06.04/projetoNode/routes/index.js
+++ Aula07 - 06.04/projetoNode/routes/index.js
@@ -2,12 +2,40 @@ const express = require('express');
 
 //rotas (quando o servidor for acessado, pra onde ele vai?)
 const router = express.Router();
+
+/*
 router.get('/',(req,res)=>{
-    res.send('Informações');
+    let nome = req.query.nome;
+    let idade = req.query.idade;
+    res.send('Olá ' + nome + ', você tem: ' + idade + ' anos de idade');
 });
+*/
+
+router.get('/',(req,res)=>{
+    res.send('Ola ' + req.query.nome + ', sua idade é: ' + req.query.idade);
+});
+
+/*
+router.get('/post/:id', (req,res)=>{
+    let id = req.params.id;
+    res.send('ID dos post: ' + id);
+});
+*/
+
+
+router.get('/', (req,res)=>{
+    let nome = req.query.nome;
+    let sobrenome = req.query.sobrenome;
+
+    res.json({
+        nomeCompleto:nome + ' ' + sobrenome
+    })
+}); 
+
 router.get('/contato',(req,res)=>{
     res.send('Contato');
 });
+
 router.get('/cadastro',(req,res)=>{
     res.send('Cadastro');
 });
